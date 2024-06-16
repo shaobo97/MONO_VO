e231f12bd345c1eab05cd7471b473357a351c817
shaobo97 <1159634951@qq.com>
2024-06-16 13:47:59 +0800

    使用模板类的sophus

--- include/my_slam/basics/eigen_funcs.h
+++ include/my_slam/basics/eigen_funcs.h
@@ -9,25 +9,26 @@
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 
-#include <sophus/so3.h>
-#include <sophus/se3.h>
+#include <sophus/se3.hpp>
+#include <sophus/so3.hpp>
 
 #include <opencv2/core/eigen.hpp>
 
-namespace my_slam
-{
-namespace basics
-{
+namespace my_slam {
+namespace basics {
 
 // --------------  Eigen --------------
 
-// Get Affine3d using pos (x,y,z) and axis-rotation (rot_axis_x, rot_axis_y, rot_axis_z) with maginitude
-Eigen::Affine3d getAffine3d(double x, double y, double z, double rot_axis_x, double rot_axis_y, double rot_axis_z);
+// Get Affine3d using pos (x,y,z) and axis-rotation (rot_axis_x, rot_axis_y,
+// rot_axis_z) with maginitude
+Eigen::Affine3d getAffine3d(double x, double y, double z, double rot_axis_x,
+                            double rot_axis_y, double rot_axis_z);
 
 // -------------- CV <--> Eigen --------------
 
 // Convert cv::Mat R and t --> Eigen Affine3d
-Eigen::Affine3d transT_CVRt_to_EigenAffine3d(const cv::Mat &R, const cv::Mat &t);
+Eigen::Affine3d transT_CVRt_to_EigenAffine3d(const cv::Mat &R,
+                                             const cv::Mat &t);
 
 // -------------- CV <--> Sophus --------------
 Sophus::SE3 transT_cv2sophus(const cv::Mat &T_cv);
